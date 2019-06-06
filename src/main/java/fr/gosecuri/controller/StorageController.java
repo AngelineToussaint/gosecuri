746faf1042b939273d0643171baf6d8df865eae4
Adrien <adrienm.martineau@gmail.com>
2019-06-06 13:27:25 +0200

    Add changeQuality

--- src/main/java/fr/gosecuri/controller/StorageController.java
+++ src/main/java/fr/gosecuri/controller/StorageController.java
@@ -1,8 +1,13 @@
 package fr.gosecuri.controller;
 
+import com.google.firebase.database.DataSnapshot;
+import fr.gosecuri.model.Kit;
 import fr.gosecuri.model.User;
 import fr.gosecuri.view.MainPage;
 import fr.gosecuri.view.StoragePage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StorageController {
     private StoragePage storagePage;
@@ -17,7 +22,7 @@ public class StorageController {
         storagePage.getDisconnectButton().addActionListener(e -> switchToPage());
 
         storagePage.getBrassardDeSecuriteCheckBox().addActionListener(e -> {
-            // TODO : edit in db
+            changeQuantity("brassard de sécurité", storagePage.getBrassardDeSecuriteCheckBox().isSelected());
         });
 
         storagePage.getCeintureDeSecuriteTactiqueCheckBox().addActionListener(e -> {
@@ -43,6 +48,27 @@ public class StorageController {
         storagePage.getMousquetonCheckBox().addActionListener(e -> {
             // TODO : edit in db
         });
+    }
+
+    private void changeQuantity(String name, boolean isChecked) {
+        Kit kit = new Kit();
+        DataSnapshot dataSnapshot1 = kit.get("");
+
+        HashMap<String, HashMap> values = (HashMap<String, HashMap>) dataSnapshot1.getValue();
+
+        for (Map.Entry<String, HashMap> entry : values.entrySet()) {
+            if (entry.getValue().get("name").equals(name)) {
+                if(isChecked) {
+                    kit.store(
+                            entry.getKey(), "quantity", (Integer.parseInt(entry.getValue().get("quantity").toString()) + 1)
+                    );
+                } else {
+                    kit.store(
+                            entry.getKey(), "quantity", (Integer.parseInt(entry.getValue().get("quantity").toString()) - 1)
+                    );
+                }
+            }
+        }
     }
 
     private void switchToPage() {
